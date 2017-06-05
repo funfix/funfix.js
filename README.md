5e0ac86f05e26eb07d8c69dcc61ab91f9cec30d5
Alexandru Nedelcu <noreply@alexn.org>
2017-06-05 22:10:10 +0300

    chore: setup travis for semantic releases

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # funcat.js
 
 [![Travis](https://img.shields.io/travis/alexandru/funcat.svg)](https://travis-ci.org/alexandru/funcat)
+[![Coverage Status](https://coveralls.io/repos/github/alexandru/funcat/badge.svg?branch=master)](https://coveralls.io/github/alexandru/funcat?branch=master)
 
 Funcat is a library which provides data types for functional
 programming in Javascript.
---
+++ tools/gh-pages-publish.ts
@@ -0,0 +1,31 @@
+import { cd, exec, echo, touch } from "shelljs"
+import { readFileSync } from "fs"
+import * as url from "url"
+
+let repoUrl
+let pkg = JSON.parse(readFileSync("package.json") as any)
+if (typeof pkg.repository === "object") {
+  if (!pkg.repository.hasOwnProperty("url")) {
+    throw new Error("URL does not exist in repository section")
+  }
+  repoUrl = pkg.repository.url
+} else {
+  repoUrl = pkg.repository
+}
+
+let parsedUrl: url.Url = url.parse(repoUrl)
+let repository = parsedUrl.host || "" + parsedUrl.path || ""
+let ghToken = process.env.GH_TOKEN
+
+echo("Deploying docs!!!")
+cd("dist/docs")
+touch(".nojekyll")
+exec("git init")
+exec("git add .")
+exec('git config user.name "Alexandru Nedelcu"')
+exec('git config user.email "noreply@alexn.org"')
+exec('git commit -m "docs(docs): update gh-pages"')
+exec(
+  `git push --force --quiet "https://${ghToken}@${repository}" master:gh-pages`
+)
+echo("Docs deployed!!")
