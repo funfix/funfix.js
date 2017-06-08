9f61f2bac866eafca5daf1d22e4ea77326ae2e2b
Alexandru Nedelcu <noreply@alexn.org>
2017-06-08 10:53:34 +0300

    docs: Change script that auto-publishes to gh-pages to do subdir versioning

--- tools/gh-pages-publish.ts
+++ tools/gh-pages-publish.ts
@@ -1,6 +1,9 @@
+#!/usr/bin/env ts-node
+
 import { cd, exec, echo, touch } from "shelljs"
 import { readFileSync } from "fs"
 import * as url from "url"
+import * as path from "path"
 
 let repoUrl
 let pkg = JSON.parse(readFileSync("package.json") as any)
@@ -13,14 +16,28 @@ if (typeof pkg.repository === "object") {
   repoUrl = pkg.repository
 }
 
-let parsedUrl: url.Url = url.parse(repoUrl)
-let repository = (parsedUrl.host || "") + (parsedUrl.path || "")
-let ghToken = process.env.GH_TOKEN
+const parsedUrl: url.Url = url.parse(repoUrl)
+const repository = (parsedUrl.host || "") + (parsedUrl.path || "")
+const ghToken = process.env.GH_TOKEN
+
+const destDir = `${pkg.name}-docs`
+const sourceDir = path.resolve(".")
+const version = exec("git fetch && git tag | grep '^v' | sort | tail -1")
+  .toString()
+  .replace(/^\s+|\s+$/g, "")
 
 echo("Deploying docs!!!")
-cd("dist/docs")
-touch(".nojekyll")
-exec("git init")
+cd(`${process.env.TMPDIR}`)
+
+exec(`rm -rf "${destDir}"`)
+exec(`git clone "https://${ghToken}@${repository}" "${destDir}"`)
+
+exec(`mkdir -p "${destDir}"/api/`)
+exec(`rm -rf "${destDir}"/api/${version} && rm -rf "${destDir}"/api/latest`)
+exec(`rsync -rcv --delete-excluded "${sourceDir}"/dist/docs/ "${destDir}"/api/${version}`)
+exec(`ln -s ./${version} "${destDir}"/api/latest`)
+
+cd(destDir)
 exec("git add .")
 exec('git config user.name "Alexandru Nedelcu"')
 exec('git config user.email "noreply@alexn.org"')
