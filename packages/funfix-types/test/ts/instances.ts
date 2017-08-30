7bc77e4649763377285adefe44cfe098ce899feb
Alexandru Nedelcu <noreply@alexn.org>
2017-08-30 10:27:45 +0300

    fix(core): tests

--- packages/funfix-types/test/ts/instances.ts
+++ packages/funfix-types/test/ts/instances.ts
@@ -53,7 +53,7 @@ export const arbOptNonempty: jv.Arbitrary<Option<number>> =
 export const arbEither: jv.Arbitrary<Either<number, number>> =
   jv.number.smap(
     i => i % 4 < 3 ? Right(i) : Left(i),
-    (fa: Either<number, number>) => fa.isRight() ? fa.get() : fa.left().get()
+    (fa: Either<number, number>) => fa.isRight() ? fa.get() : fa.swap().get()
   )
 
 export const arbSuccess: jv.Arbitrary<Try<number>> =
